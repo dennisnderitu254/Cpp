26286e44562aaa8480fdb86eeda866b57c806780
Dennis Nderitu <dknderitu@gmail.com>
2017-09-24 21:48:41 +0300

    more simple arithmetic

--- 05_cpp.cpp
+++ 05_cpp.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 
 using namespace std;
@@ -7,15 +8,15 @@ int main()
     int a;
     int b;
     int sum;
-    
+
     cout << "Enter a number hoss! \n";
     cin >> a;
-    
+
     cout << "Enter another number \n";
     cin >> b;
-    
+
     sum = a+b;
     cout << "the sum of those numbers is " << sum << endl;
-    
+
     return 0;
 }
