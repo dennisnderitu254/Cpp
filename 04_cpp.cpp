438fa9bcb276329a8f07f8e2491b93ea788d7c07
Dennis Nderitu <dknderitu@gmail.com>
2017-09-24 21:45:23 +0300

    simple arithmetic operation

---
+++ 04_cpp.cpp
@@ -0,0 +1,17 @@
+// simple arithmetic function
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+
+  int a = 4;
+  int b = 21;
+
+  int sum = a + b;
+
+  cout << sum;
+  return 0;
+}
