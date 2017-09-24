ac337f2c33329f70517616263ac39a67d3289ed7
Dennis Nderitu <dknderitu@gmail.com>
2017-09-24 22:40:10 +0300

    Using functions that take parameters

---
+++ 10_cpp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// This function takes a parameter.
+void printCrap(int x){
+    cout << "buckys favorite number is " << x << endl;
+}
+
+int main()
+{
+    printCrap(854); // Call the function.
+
+    return 0;
+}
