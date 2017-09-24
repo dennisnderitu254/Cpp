f5b3a7647b8811f42e5c0bd8c67880266e109795
Dennis Nderitu <dknderitu@gmail.com>
2017-09-24 15:07:25 +0300

    Program that does simple arithmetic

---
+++ arithmetic.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+  float a,b;
+
+  cout<<"Enter two numbers"<<endl;
+  cin>>a>>b;
+  cout<<"The addition is:"<<a+b<<endl;
+  cout<<"The multiplication is:"<<a+b<<endl;
+  cout<<"The division is:"<<a/b<<endl;
+
+  return 0;
+
+}
