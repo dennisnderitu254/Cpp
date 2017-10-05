c721523f72b49c2c267027c5730e2ebf795c5015
Dennis Nderitu <dknderitu@gmail.com>
2017-10-05 15:11:32 +0300

    variable declaration

---
+++ ex2.cpp
@@ -0,0 +1,25 @@
+// Variable Example
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Definition of Variables
+
+	int a,b,c;
+	float e;
+
+	// initializing of variables
+
+	a=5;
+	b=10;
+	c=a+b;
+
+	cout<<c<<endl;
+
+	e = 15.0/4.0;
+	cout<<e<<endl;
+
+	return 0;
+}
