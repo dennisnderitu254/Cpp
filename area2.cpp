623fde3a1d6de964286c1774ee61ba7fe1146997
Dennis Nderitu <dknderitu@gmail.com>
2017-10-05 14:28:56 +0300

    area calculation using functions

---
+++ area2.cpp
@@ -0,0 +1,18 @@
+// Computing Area using structured method
+
+#include <iostream>
+using namespace std;
+#define pie 3.14
+
+double area(double r)
+{
+	return(pie*r*r);
+}
+
+int main()
+{
+	double radius=3;
+	cout << area(3);
+
+	return 0;
+}
