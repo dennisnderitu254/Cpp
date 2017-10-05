d38a450b2c73034d493bb639fd19ce95c2d87ff5
Dennis Nderitu <dknderitu@gmail.com>
2017-10-05 15:34:41 +0300

    local and global variable declaration

---
+++ ex3.cpp
@@ -0,0 +1,16 @@
+// Local and global variable declaration
+
+#include <iostream>
+using namespace std;
+
+// Global variable declaration
+int a=20;
+
+int main()
+{
+	//local variable declaration
+	int a=10;
+
+	cout<<a;
+	return 0;
+}
