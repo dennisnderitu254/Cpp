1712c65d0c0245c4efabf9ccb1d7096c431f9986
Dennis Nderitu <dknderitu@gmail.com>
2017-10-05 14:58:31 +0300

    while looping

---
+++ ex14.cpp
@@ -0,0 +1,20 @@
+// While Loops
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// local variable declaration
+	int a=1;
+
+	// While loop execution
+
+	while(a<6)
+	{
+		cout<<"Value of a:"<<a<<endl;
+		a=a+1;
+	}
+
+	return 0;
+}
