9eff45e38ba738416be7453753774eace0b77bc4
Dennis Nderitu <dknderitu@gmail.com>
2017-10-05 15:33:19 +0300

    number reversal using an array

---
+++ ex29.cpp
@@ -0,0 +1,24 @@
+// Program that enables revesal of a number using an Array
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n,array[100],i;
+	cout<<"Enter no of digits in your number";
+	cin>>n;
+
+
+	cout<<"Enter no.";
+	for(i=0;i<n;i++)
+		cin>>a[i];
+		// storing each number in array
+
+	cout<<"Reverse no is:";
+	for(i=n-1;i>=0;i--)
+		cout<<a[i];
+		// printing an array in reverse order
+
+	return 0;
+}
