cddbbb322ef183d99fb9dc3a9f097cc01d8bc587
Dennis Nderitu <dknderitu@gmail.com>
2017-10-05 15:07:20 +0300

    area of a triangle

---
+++ ex17.cpp
@@ -0,0 +1,19 @@
+// Area of Triangle
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int height,base;
+	float ans;// ans may come in fractions
+
+	cout<<"Enter height and Base\n";
+	cin>>height>>base;
+	ans=(0.5)*(height*base);
+	// mathematical formula
+
+	cout<<"Area if trangle \n"<<ans;
+
+	return 0;
+}
