0fc6e98908f4aaee2002c477f0d199f3f8709029
Dennis Nderitu <dknderitu@gmail.com>
2017-10-05 15:40:54 +0300

    addition of two numbers

---
+++ ex5.cpp
@@ -0,0 +1,19 @@
+// Addition of two numbers
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int first,second,sum;
+
+	cout<<"Enter two integers to add\n";
+	cin>>first>>second;
+
+	sum = first+second;
+	/*Adding contents of first and second and storing in sum*/
+
+	cout<<"Sum of Entered numbers = "<<sum<<"\n";
+
+	return 0;
+}
