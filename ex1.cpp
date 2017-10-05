cb0517cea0998ff47abd15f576fcefc5452b5ae8
Dennis Nderitu <dknderitu@gmail.com>
2017-10-05 14:35:28 +0300

    first C++ program

---
+++ ex1.cpp
@@ -0,0 +1,15 @@
+// Hello World
+
+#include <iostream>
+using namespace std;
+
+// tells compiler to include std input output header file
+
+int main()
+{
+	cout<<"Hello world\n";
+	// prints Hello World on user screen
+
+	return 0;
+	// return the value zero to the OS
+}
