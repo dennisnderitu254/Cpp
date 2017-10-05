55028ad3d62469cefd6fd32efe14d5e19d883cfd
Dennis Nderitu <dknderitu@gmail.com>
2017-10-05 15:32:13 +0300

    program that computes the reverse number

---
+++ ex28.cpp
@@ -0,0 +1,25 @@
+// Program tha computes the reverse number
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n,reverse=0;
+
+	cout<<"Enter a number to reverse\n";
+	cin>>n;
+
+	while(n!=0)
+	{
+		reverse=reverse*10;
+		reverse=reverse+n%10;
+		n=n/10;
+	}
+
+	/* Taking unit place of no and moving to reverse Dividing the no to discard unit place digit */
+
+	cout<<"Reverse of Entered number is ="<<reverse<<"\n";
+
+	return 0;
+}
