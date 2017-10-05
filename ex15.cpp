167228379577bae1e69a6eff1cb3ccba6dd69b03
Dennis Nderitu <dknderitu@gmail.com>
2017-10-05 15:00:05 +0300

    do while loops

---
+++ ex15.cpp
@@ -0,0 +1,18 @@
+// Do-While loop Example
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//local variable declaration
+	int a=5;
+	// do-while loop execution
+	do
+	{
+		cout<<"Value of a:"<<a<<endl;
+		a=a+1;
+	}while(a<20);
+
+	return 0;
+}
