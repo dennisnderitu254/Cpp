ef73aa913dceee378775d1af5597a001189b0747
Dennis Nderitu <dknderitu@gmail.com>
2017-10-05 15:44:50 +0300

    if-statements

---
+++ ex8.cpp
@@ -0,0 +1,21 @@
+// If statements C++ Edition
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a=10;
+
+	// check for condition
+	if(a>5)
+	{
+		// if condition is true then print the following
+
+		cout<<"If condition is Satisfied";
+		cout<<"a is greater than 5"<<endl;
+	}
+
+	cout<<"Value of a is:"<<a<<endl;
+
+	return 0;
+}
