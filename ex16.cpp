c3f13d8291d13ab45bfa9c8602fe7efad1480965
Dennis Nderitu <dknderitu@gmail.com>
2017-10-05 15:05:48 +0300

    for looping

---
+++ ex16.cpp
@@ -0,0 +1,16 @@
+// For-Loops
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//local variable declaration
+	int a=1;
+	// while loop execution
+	for(a=1;a<6;a++)
+	{
+		cout<<"Value of a:"<<a<<endl;
+	}
+
+	return 0;
+}
