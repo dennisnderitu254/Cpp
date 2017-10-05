80839e7344bda5d03292af986eedafcacc487ca8
Dennis Nderitu <dknderitu@gmail.com>
2017-10-05 15:39:45 +0300

    print integer

---
+++ ex4.cpp
@@ -0,0 +1,18 @@
+// Print integer
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+
+	cout<<"Enter an Integer\n";
+	cin>>a;
+
+	// takes an integer from user
+
+	cout<<"Integer that you have entered is"<<a<<"\n";
+
+	return 0;
+}
