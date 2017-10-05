412a74c5b9f97128fb3f213433c57f1eef3d6850
Dennis Nderitu <dknderitu@gmail.com>
2017-10-05 14:39:28 +0300

    using If-else statements

---
+++ ex10.cpp
@@ -0,0 +1,30 @@
+// if-else condition
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a=10;
+
+	// check for condition
+
+	if(a>20)
+	{
+		// if condition is true then print the following
+
+		cout<<"If Condition is  satisfied";
+		cout<<"a is greater than 20"<<endl;
+	}
+
+	else
+	{
+		// If condition is false then print the following
+
+		cout<<"If Condition is not satisfied";
+		cout<<"  a is not greater than 20"<<endl;
+	}
+
+	cout<<"Value of a is:"<<a<<endl;
+
+	return 0;
+}
