1b8d7787b0777e6562fc4bd417e41b2f46318dec
Dennis Nderitu <dknderitu@gmail.com>
2017-10-08 21:40:54 +0300

    program that checks for prime numbers

---
+++ ex31.cpp
@@ -0,0 +1,44 @@
+// program that checks for prime numbers
+// review code again
+
+#include <iostream>
+using namespace std;
+
+int check_prime(int);
+
+int main()
+{
+	int i,n,result;
+
+	cout<<"Enter the number of prime numbers required\n";
+	cin>>n;
+	cout<<"First "<<n<<" prime numbers are:\n";
+
+	for(i=0;i<n;i++){
+
+        result=check_prime(i);
+        // if i is prime then it will return 1;
+
+
+        if(result == 1)
+		cout<<i<<"\n";
+
+	}
+
+
+	return 0;
+}
+
+int check_prime(int a)
+{
+	int c;
+	/* starting from 2, if no is completely divisible by any no then it is not prime*/
+	for(c=2;c<=a-1;c++)
+	{
+		if(a%c==0)
+			return 0;
+	}
+
+	if(c==a)
+		return 1;
+}
