40ed2da0666b0783ae388d0534cc97e77fc5e8e5
dennisnderitu254 <dknderitu@gmail.com>
2017-10-16 11:22:41 +0300

    decimal to octal conversion

---
+++ ex46.cpp
@@ -0,0 +1,27 @@
+// Program that converts Decimal to Octal
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	long num,decimal_num,remainder,base=1,octal=0;
+
+	cout<<"Enter a decimal integer\n";
+	cin>>num;
+
+	decimal_num = num;
+
+	while(num > 0)
+	{
+		remainder = num % 8;
+		octal=octal+remainder*base;
+		num=num/8;
+		base=base*10;
+	}
+
+	cout<<"Input number is ="<<decimal_num<<"\n";
+	cout<<"Its octal equivalent is ="<<octal<<"\n";
+
+	return 0;
+}
