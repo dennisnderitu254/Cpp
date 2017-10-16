f1d358a1487a9c3e3a6754d95f2279bb5dd2969f
dennisnderitu254 <dknderitu@gmail.com>
2017-10-16 11:28:27 +0300

    Writing a 2D array

---
+++ ex51.cpp
@@ -0,0 +1,40 @@
+// 2D Array Example
+
+#include <iostream>
+#define size 16
+using namespace std;
+
+int main()
+{
+	int m,n;
+	int a[size][size];
+
+	cout<<"Enter the number of rows"<<endl;
+	cin>>m;
+	cout<<"Enter the number of columns"<<endl;
+	cin>>n;
+
+	cout<<"Enter the Elements in Table"<<endl;
+
+	for(int i=0;i < m;i++)
+	{
+		for(int j=0; j< n;j++)
+		{
+			cin>>a[i][j];
+		}
+	}
+
+	// Output each array element's value
+
+	for(int i=0;i < m;i++)
+	{
+		for(int j = 0;j < n;j++)
+		{
+			cout<<"a["<<i<<"]["<<j<<"]:";
+			cout<<a[i][j]<<endl;
+		}
+	}
+		
+
+		return 0;
+}
