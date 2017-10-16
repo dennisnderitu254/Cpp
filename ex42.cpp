eb02df8d6dc7c3813d1714110a1ee4a6e62fa8e4
dennisnderitu254 <dknderitu@gmail.com>
2017-10-16 11:16:37 +0300

    Printing pascals triangle

---
+++ ex42.cpp
@@ -0,0 +1,38 @@
+// Print Pascals triangle
+#include <iostream>
+using namespace std;
+
+
+long factorial(int);
+
+int main()
+{
+	int i,n,c;
+
+	cout<<"Enter the number of rows you wish to see in pascals triangle\n";
+	cin>>n;
+
+	for(i=0;i<n;i++)
+	{
+		for(c=0;c<=(n-i-2);c++)
+			cout<<" ";
+		for(c=0;c<=i;c++)
+			cout<<factorial(i)/(factorial(c)*factorial(i-c));
+
+		cout<<"\n";
+	}
+
+	return 0;
+}
+
+long factorial(int n)
+{
+	int c;
+	long result=1;
+
+	for(c=1;c<=n;c++)
+		result=result*c;
+
+	return(result);
+}
+
