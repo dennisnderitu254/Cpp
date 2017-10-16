c3f1e7b973b93bdeefd92420b7c6858b9b3eeda6
dennisnderitu254 <dknderitu@gmail.com>
2017-10-16 11:18:12 +0300

    Program that converts decimal to bitwise

---
+++ ex43.cpp
@@ -0,0 +1,27 @@
+// Program that converts Decimal to Bitwise
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n,c,k;
+
+	cout<<"Enter an integer in decimal number system \n";
+	cin>>n;
+
+	cout<<n<<"In binary number system is:\n";
+
+	for(c=31;c>=0;c--)
+	{
+		k=n>>c;
+		// right shift(Binary Divide by 2)
+
+		if(k&1) // k is logically ANDed with 1
+			cout<<"1";
+		else
+			cout<<"0";
+	}
+
+	return 0;
+}
