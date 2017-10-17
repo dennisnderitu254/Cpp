44072ae6f64eef71bf509face0f4df276fa58275
dennisnderitu254 <dknderitu@gmail.com>
2017-10-17 23:22:14 +0300

    utilizing methods

---
+++ ex54.cpp
@@ -0,0 +1,59 @@
+// Public member Function
+
+#include <iostream>
+
+using namespace std;
+
+class Rectangle
+{
+public:
+	double length;
+	double breadth;
+	void setLength(double l);
+	double getLength(void);
+	void setBreadth(double b);
+	double getBreadth(void);
+};
+
+// Member functions definitions
+double Rectangle::getLength(void)
+{
+	return length;
+}
+
+void Rectangle::setLength(double l)
+{
+	length = l;
+}
+
+double Rectangle::getBreadth(void)
+{
+	return breadth;
+}
+
+double Rectangle::setBreadth(double b)
+{
+	breadth=b;
+}
+
+// Main function for the program
+int main()
+{
+	Rectangle R;
+
+	// set Rectangle length and Breadth
+	R.setlength(5.0);
+	R.setBreadth(4.0);
+	cout<<"Length of rectangle:"<<R.getLength()<<endl;
+	cout<<"Length of rectangle:"<<R.getBreadth()<<endl;
+
+
+	// set Rectangle Length And Breadth without member function
+	R.length = 10.0;
+	R.breadth = 8.0;
+	cout<<"Length of Rectangle :"<<R.length<<endl;
+	cout<<"Breadth of Rectangle :"<<R.breadth<<endl;
+
+	return 0;
+
+}
